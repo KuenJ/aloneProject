069eb8e1013015a4e142e8dd9da72b33f54db834
kai_lee <krsk501@kakao.com>
2024-03-05 13:22:27 +0900

    search and design update

--- main.js
+++ main.js
@@ -15,51 +15,79 @@ buttonSpace.forEach(
     )
 );
 
-const getApi = async () => {
-  const url = new URL(
-    `http://apis.data.go.kr/1262000/TravelAlarmService2/getTravelAlarmList2?ServiceKey=${API_KEY}`
-  );
+let url = new URL(
+  `http://apis.data.go.kr/1262000/TravelAlarmService2/getTravelAlarmList2?ServiceKey=${API_KEY}` // 원래는 getApi함수에서 사용되었으나  다 공통으로 쓰이기때문에 전역변수로 선언후 url 변수만가져다쓰면됨아래는
+);
+
+const getApiInformation = async () => {
+  //위 코드는 아래에서 계속반복되는 url  가져와서 json 변환해서 데이터 담아서  화면에 그려주는 역할을 하나의 함수정의해서 함수만선언해서 간편하게 사용가능
   const response = await fetch(url);
   const data = await response.json();
 
-  InfoList = data.data;
+  InfoList = data.data; //  업데이트 한내용을 infoList 배열에 다시넣고 그려줘야 새롭게  클릭시 해당하는 값들이나온다.
   render();
-  console.log("dddd", InfoList);
+};
+
+const getApi = async () => {
+  url = new URL(
+    `http://apis.data.go.kr/1262000/TravelAlarmService2/getTravelAlarmList2?ServiceKey=${API_KEY}`
+  );
+  getApiInformation();
 };
 
 const getContinentNameInformation = async (event) => {
   // 버튼에 해당하는 카테고리에  따라서 가져오는 코드
   const category = event.target.textContent;
   console.log(category);
-  const url = new URL(
+  url = new URL(
     `http://apis.data.go.kr/1262000/TravelAlarmService2/getTravelAlarmList2?cond[continent_eng_nm::EQ]=${category}&ServiceKey=${API_KEY}`
   );
+
   const response = await fetch(url);
   const data = await response.json();
-  console.log("Ddd", data);
+
   InfoList = data.data; //  업데이트 한내용을 infoList 배열에 다시넣고 그려줘야 새롭게  클릭시 해당하는 값들이나온다.
   render();
+  console.log("Ddd", data);
 };
 
 const getCountryBySearch = async () => {
-  const search = document.getElementById("search-input").value;
+  //검색으로  조회에서 가져오는 코드 .
+  const search = document.getElementById("search-input").value; //input 검색창에 value값을
   console.log("search", search);
 
-  const url = new URL(
-    `http://apis.data.go.kr/1262000/TravelAlarmService2/getTravelAlarmList2?cond[country_nm::EQ]=${search}&ServiceKey=${API_KEY}`
-  );
+  const isKorean = /[ㄱ-ㅎㅏ-ㅣ가-힣]/g.test(search); //먼저, 여러분이 작성한 코드에서 한글과 영어를 판단하는 부분을 살펴보겠습니다. 현재 코드에서 isKorean 변수는 정규식 /[ㄱ-ㅎㅏ-ㅣ가-힣]/g를 사용하여 한글 여부를 확인하고 있습니다. 그러나 이 부분에는 작은 문제가 있습니다.
+  //정규식을 사용할 때는 test() 함수를 호출하여 해당 정규식이 문자열에 존재하는지 여부를 확인해야 합니다.
+
+  if (isKorean) {
+    url = new URL(
+      `http://apis.data.go.kr/1262000/TravelAlarmService2/getTravelAlarmList2?cond[country_nm::EQ]=${search}&ServiceKey=${API_KEY}`
+    );
+  } else {
+    url = new URL(
+      `http://apis.data.go.kr/1262000/TravelAlarmService2/getTravelAlarmList2?cond[country_eng_nm::EQ]=${search}&ServiceKey=${API_KEY}`
+    );
+  }
+
   const response = await fetch(url);
   const data = await response.json();
-  console.log("sarcomata", data);
-  InfoList = data.data;
+
+  InfoList = data.data; //  업데이트 한내용을 infoList 배열에 다시넣고 그려줘야 새롭게  클릭시 해당하는 값들이나온다.
   render();
+  console.log("sarcomata", data);
 };
+
 const render = () => {
-  const newInfo = InfoList.map((info) => {
-    return `<div class="row informal">
+  const newInfo = InfoList.map(
+    (info) =>
+      `<div class="row informal">
         <div class="col-lg-8">
-          <img class="flag-img-size" src=${info.flag_download_url} " />
-          <img class="map-img-size" src=${info.map_download_url} " />
+        <section class="flag-box">
+          <img  src=${info.flag_download_url} " />
+        </section>
+        <section class="country-box">  
+          <img  src=${info.map_download_url} " />
+        </section>
         </div>
         <div class="col-lg-4">
           <ul class="list-unstyled">
@@ -67,14 +95,14 @@ const render = () => {
             <li>한글 국가명: ${info.country_nm}</li>
             <li>한글 대륙명: ${info.continent_nm}</li>
             <li>영문 대륙명: ${info.continent_eng_nm}</li>
-            <li>경보 단계: ${info.alarm_lvl}</li>
-            <li>작성일:${info.written_dt}</li>
-            <li>위험지역:${info.remark}</li>
+            <li>경보 단계: ${info.alarm_lvl || "없음"}</li>   
+            <li>작성일: ${info.written_dt}</li>
+            <li>위험지역: ${info.remark || "없음"}</li>
           </ul>
         </div>
-      </div>`;
-  }).join("");
-
+      </div>`
+  ).join("");
+  //  || 없음으로 null값은 없음으로처리할수있게되었다.
   document.getElementById("information-board").innerHTML = newInfo;
 };
 
